46babb99e9bdb6fb528ce667774031237e2efdfb
sravya <sravya.kodehode@gmail.com>
2025-02-15 23:54:57 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/wishlist_react/", // Replace with your repo name
+  base: "/wishlist_react/", // Ensure this is correct for your GitHub Pages repo
 });
