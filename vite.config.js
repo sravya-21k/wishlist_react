523c2e0babf2a759e08255b8a66667eb428bec11
sravya <sravya.kodehode@gmail.com>
2025-02-16 00:53:35 +0100

    almost ferdig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/wishlist_react/", // Add this line for GitHub Pages
   build: {
     assetsDir: "assets", // Ensure assets are placed under the 'assets' directory
+    outDir: "dist", // This is typically default, but good to explicitly specify
   },
 });
